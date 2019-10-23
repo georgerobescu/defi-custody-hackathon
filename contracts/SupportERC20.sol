43387493907dce985850efa853021b22d640b4f8
IlgizZ <ilgiz1996@mail.ru>
2019-10-22 17:29:20 -0700

    add simple erc20 and tests for SupportERC20

--- contracts/SupportERC20.sol
+++ contracts/SupportERC20.sol
@@ -2,11 +2,14 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import '@openzeppelin/upgrades/contracts/utils/Address.sol';
 
 contract SupportERC20 is Ownable {
     IERC20 public erc20;
 
-    constructor (IERC20 _erc20) {
+    event TokenUpdate(IERC20 indexed oldImpl, IERC20 indexed newImpl);
+
+    constructor (IERC20 _erc20) public {
         erc20 = _erc20;
     }
 
@@ -19,6 +22,8 @@ contract SupportERC20 is Ownable {
     }
 
     function setERC20(IERC20 _erc20) public onlyOwner {
+        require(OpenZeppelinUpgradesAddress.isContract(address(_erc20)), "Cannot set a ERC20 implementation to a non-contract address");
+        emit TokenUpdate(erc20, _erc20);
         erc20 = _erc20;
     }
 }
