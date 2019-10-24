7f413e620da4653d8aa0f0eb8339d95ce667986e
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-10-24 16:37:10 -0700

    Make RecoveryLogic a monolith contract

--- contracts/RecoveryLogic.sol
+++ contracts/RecoveryLogic.sol
@@ -3,20 +3,24 @@ pragma solidity >=0.5.0 <0.6.0;
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract RecoveryLogic is Ownable {
-    uint256 public lastAction;
-    uint256 public recoveryDeadline;
-    address[] public recoveryWallets;
+    address[] public DCwalletsList;
+    
+    mapping(address => uint256) public lastAction;
+    mapping(address => uint256) public recoveryDeadline;
+    mapping(address => address[]) public recoveryWallets;
 
-    // recoveryWallet => asset address => percentage | 1 ETH = 100%
-    mapping(address => mapping(address => uint256)) public recoverySheet;
+    // wallet => recoveryWallet => asset address => percentage | 1 ETH = 100%
+    mapping(address => mapping(address => mapping(address => uint256))) public recoverySheet;
 
-    modifier updateAction() {
-        lastAction = now;
+    event NewRecoverySheet(address user, address asset, address[] wallets, uint256[] values, uint256 deadline);
+
+    modifier updateAction(address wallet) {
+        lastAction[wallet] = now;
         _;
     }
 
-    modifier ifRecoverable() {
-        require(isRecoverable(), "Contract is not recoveralbe");
+    modifier ifRecoverable(address wallet) {
+        require(isRecoverable(wallet), "#RecoveryLogic ifRecoverable() modifier: Contract is not recoverable");
         _;
     }
 
@@ -32,32 +36,29 @@ contract RecoveryLogic is Ownable {
         address[] memory wallets,
         uint256[] memory values,
         uint256 deadline
-    ) public onlyOwner updateAction {
-        require(asset != address(0), "Asset cannot be zero address");
-        require(deadline > 0, "Deadline must be bigger than zero");
-        require(wallets.length == values.length && wallets.length > 0, "Length incorrect. Data corrupted");
+    ) public updateAction(msg.sender) {
+        require(asset != address(0), "#RecoveryLogic setRecoverySheet(): Asset cannot be zero address");
+        require(deadline > 0, "#RecoveryLogic setRecoverySheet(): Deadline must be bigger than zero");
+        require(wallets.length == values.length && wallets.length > 0, "#RecoveryLogic setRecoverySheet(): Length incorrect. Data corrupted");
         uint valuesTotalAmount = 0;
         for (uint256 i = 0; i < wallets.length; i++) {
-            require(wallets[i] != address(0), "Recovery wallet cannot be zero address");
-            require(values[i] <= 1 ether, "Percentage must be smaller then 1 ether(100%)");
-            recoverySheet[wallets[i]][asset] = values[i];
+            require(wallets[i] != address(0), "#RecoveryLogic setRecoverySheet(): Recovery wallet cannot be zero address");
+            require(values[i] <= 1 ether, "#RecoveryLogic setRecoverySheet(): Percentage must be smaller then 1 ether(100%)");
+            recoverySheet[msg.sender][wallets[i]][asset] = values[i];
             valuesTotalAmount += values[i];
         }
-        require(valuesTotalAmount == 1 ether, "Sum of all the percentages is not 1 ether(100%)");
-        recoveryDeadline = deadline;
-        recoveryWallets = wallets;
+        require(valuesTotalAmount == 1 ether, "#RecoveryLogic setRecoverySheet(): Sum of all the percentages is not 1 ether(100%)");
+        recoveryDeadline[msg.sender] = deadline;
+        recoveryWallets[msg.sender] = wallets;
+        emit NewRecoverySheet(msg.sender, asset, wallets, values, deadline);
+        DCwalletsList.push(msg.sender);
     }
 
-    function isRecoverable() public view returns (bool) {
-        return (lastAction + recoveryDeadline) <= now;
+    function isRecoverable(address wallet) public view returns (bool) {
+        return (lastAction[wallet] + recoveryDeadline[wallet]) <= now;
     }
 
-    function recover() public ifRecoverable returns (bool) {
-        // implement recovery logic
-        return true;
-    }
-
-    function getRecoveryWallets() public view returns (address[] memory) {
-        return recoveryWallets;
+    function getRecoveryWallets(address wallet) public view returns (address[] memory) {
+        return recoveryWallets[wallet];
     }
 }
