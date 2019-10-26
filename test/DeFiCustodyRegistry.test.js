2af582b1e2ecbf797027920cbba350bbec7a7dbe
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-10-26 15:38:12 -0700

    tests for DeFiCustodyRegistry

--- test/DeFiCustodyRegistry.test.js
+++ test/DeFiCustodyRegistry.test.js
@@ -8,6 +8,7 @@ const DeFiCustodyRegistry = Contracts.getFromLocal("DeFiCustodyRegistry");
 const ERC20 = artifacts.require("./MockedERC20.sol");
 
 const { expectRevert } = require("openzeppelin-test-helpers");
+const { sleep } = require("./utils/utils.js");
 
 // RAY
 const RAYUtils = require('./helpers/RAYUtils.js');
@@ -20,10 +21,12 @@ const DaiPortfolioIds = Constants.PORTFOLIO_IDS.DAI;
 contract("DeFiCustodyRegistry", async accounts => {
 
   let project;
+  const watchtower = accounts[0];
   const admin = accounts[1];
   const owner = accounts[2];
   const user1 = accounts[3];
   const nonOwner = accounts[4];
+  const recoveryWallets = [accounts[5], accounts[6]];
   const gas = 6000000;
   const rayStorage = Deployed.STORAGE;
   let deFiCustodyRegistryInstance, erc20Instance;
@@ -75,6 +78,80 @@ contract("DeFiCustodyRegistry", async accounts => {
   });
 
   describe("recoverRAY()", () => {
-    it("should recover funds");
+    it("should recover funds", async () => {
+      // invest 1 DAI
+      let value = '1';
+      await Coins.getDAI(user1, value);
+      await Coins.approveDAI(deFiCustodyRegistryInstance.address, user1, value);
+
+      let tx = await deFiCustodyRegistryInstance.methods.mint(
+        DaiPortfolioIds.BZX_COMPOUND_DYDX,
+        user1,
+        web3.utils.toWei(value, 'ether')
+      ).send({ from: watchtower, value: 0, gas: 900000 });
+
+      rayTokenId = tx.events.InvestmentRAY.returnValues.rayTokenId;
+
+      let [
+        tokenValue,
+        tokenOwner
+      ] =
+      await Promise.all([
+        RAYUtils.getRAYTokenValue(DaiPortfolioIds.BZX_COMPOUND_DYDX, rayTokenId),
+        deFiCustodyRegistryInstance.methods.getTokenOwner(rayTokenId).call()
+      ]);
+
+      assert.notEqual(rayTokenId, Constants.NULL_BYTES, "rayTokenId cannot be null bytes");
+      assert.equal(tokenValue, web3.utils.toWei(value, 'ether'), "Token value is incorrect");
+      assert.equal(tokenOwner, user1, "Owner is incorrect");
+
+
+
+
+
+      // setup recovery sheet
+      const deadlineBefore = await deFiCustodyRegistryInstance.methods
+        .recoveryDeadline(user1)
+        .call();
+
+      const assetPercentage = [web3.utils.toWei("0.5", "ether"), web3.utils.toWei("0.5", "ether")];
+      const deadline = 2; //2 seconds
+
+      await deFiCustodyRegistryInstance.methods
+        .setRecoverySheet(
+          Deployed.DAI_TOKEN,
+          recoveryWallets,
+          assetPercentage,
+          deadline
+        ).send({ from: user1, gas });
+
+      const firstWalletPercentage = await deFiCustodyRegistryInstance.methods
+        .recoverySheet(user1, recoveryWallets[0], Deployed.DAI_TOKEN)
+        .call();
+      const deadlineAfter = await deFiCustodyRegistryInstance.methods
+        .recoveryDeadline(user1)
+        .call({ from: owner });
+      const recoveryWalletsContract = await deFiCustodyRegistryInstance.methods
+        .getRecoveryWallets(user1)
+        .call({ from: owner });
+
+      assert.equal(firstWalletPercentage, assetPercentage[0], "firstWalletPercentage hasn't changed."
+      );
+      assert.equal(deadlineAfter, 2, "Deadline hasn't changed.");
+      assert.deepEqual(recoveryWalletsContract, recoveryWallets, "Recovery wallets haven't been changed.");
+
+
+      // fire recover
+      await sleep(2000);
+
+      assert(await deFiCustodyRegistryInstance.methods.isRecoverable(user1).call(), "Should be recoverable");
+
+      // check balance of recovery wallets
+      let tokens = await deFiCustodyRegistryInstance.methods.getRayTokens(user1).call();
+      for (var i = 0; i < tokens.length; i++) {
+        let recoveryTx = await deFiCustodyRegistryInstance.methods.recoverRAY(tokens[i]).send({from: watchtower});
+        console.log(recoveryTx);
+      }
+    });
   });
 });
