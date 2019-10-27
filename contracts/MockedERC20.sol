77a0b9a1d9003b4dde199e7845874386e5f1f0b0
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-10-27 15:54:43 -0700

    fund account with test ERC20

--- contracts/MockedERC20.sol
+++ contracts/MockedERC20.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockedERC20 is ERC20 {
-    constructor() public {
+    constructor(address user, uint amount) public {
+        _mint(user, amount);
     }
 }
