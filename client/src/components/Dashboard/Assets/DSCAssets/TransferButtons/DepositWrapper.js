d22c7a1083c5ba010869c409de7cf962c292b7c7
IlgizZ <ilgiz1996@mail.ru>
2019-10-28 00:08:13 -0700

    add transactions toasts

--- client/src/components/Dashboard/Assets/DSCAssets/TransferButtons/DepositWrapper.js
+++ client/src/components/Dashboard/Assets/DSCAssets/TransferButtons/DepositWrapper.js
@@ -1,6 +1,10 @@
 import { inject, observer } from "mobx-react";
 import { compose } from "recompose";
 import { drizzleReactHooks } from "../../../../../drizzle";
+import { fetchDeFi } from "../../../../../blockchain/SmartContractCalls";
+
+const BZX_COMPOUND_DYDX =
+  "0x87e3990b15e1e64e3a17b0e4ebfcc4c03cc5ec64a33b442ae01ef15d9dadb575";
 
 const TransferButtonsHookWrapper = ({ DSCStore, Web3Store, children }) => {
   const { useCacheSend } = drizzleReactHooks.useDrizzle();
@@ -8,11 +12,19 @@ const TransferButtonsHookWrapper = ({ DSCStore, Web3Store, children }) => {
   const successToast = {
     successTitle: "Successfully deposited!",
     successSubtitle: receipt => {
-      console.log(receipt);
+      // const data = {
+      //   portfolioId: BZX_COMPOUND_DYDX,
+      //   payableBeneficiary: Web3Store.defaultAccount,
+      //   value: amount
+      // };
+      // const request = fetchDeFi("invest", data);
       return "mined";
     }
   };
-  return children({ transfer: send, TXObjects, successToast });
+  const transfer = async (amount, token) => {
+    await send(DSCStore.drizzle.contracts.DeFiCustodyRegistry.address, amount);
+  };
+  return children({ transfer, TXObjects, successToast });
 };
 
 export default compose(
--- client/src/components/Dashboard/Assets/DSCAssets/TransferButtons/WithdrawWrapper.js
+++ client/src/components/Dashboard/Assets/DSCAssets/TransferButtons/WithdrawWrapper.js
@@ -1,27 +1,25 @@
+import React from "react";
 import { inject, observer } from "mobx-react";
 import { compose } from "recompose";
 import { drizzleReactHooks } from "../../../../../drizzle";
+import Spinner from "../../../../utils/Spinner";
 
 const TransferButtonsHookWrapper = ({ DSCStore, Web3Store, children }) => {
-  const { useCacheSend } = drizzleReactHooks.useDrizzle();
+  const { useCacheSend, useCacheCall } = drizzleReactHooks.useDrizzle();
   const { send, TXObjects } = useCacheSend(
     "DeFiCustodyRegistry",
-    "getRayTokens"
+    "redeemAndWithdraw"
   );
-  // const rayTokenIds = await DSCStore.drizzle.contracts.DeFiCustodyRegistry.methods
-  // .getRayTokens(Web3Store.defaultAccount)
-  // .call();
+  const rayTokenIds = useCacheCall(
+    "DeFiCustodyRegistry",
+    "getRayTokens",
+    Web3Store.defaultAccount
+  );
   const transfer = amount => {
     amount = new Web3Store.web3.utils.BN(amount.toString()).abs();
-
     //TODO fix to range
-    const rayTokenId = 1; // rayTokenIds[0];
-    console.log(
-      "Smart contract call, withdraw: ",
-      rayTokenId,
-      amount.toString(),
-      Web3Store.defaultAccount
-    );
+    const rayTokenId = rayTokenIds[0];
+    send(rayTokenId, amount.toString(), Web3Store.defaultAccount);
   };
 
   const successToast = {
@@ -31,7 +29,11 @@ const TransferButtonsHookWrapper = ({ DSCStore, Web3Store, children }) => {
       return "mined";
     }
   };
-  return children({ transfer, TXObjects, successToast });
+  return !rayTokenIds ? (
+    <Spinner />
+  ) : (
+    children({ transfer, TXObjects, successToast })
+  );
 };
 
 export default compose(
