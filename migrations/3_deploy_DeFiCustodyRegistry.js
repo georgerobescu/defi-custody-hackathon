3c8f1533724a5b1be79eb1bdcaad7e1c09c46ad4
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-11-01 20:02:14 -0700

    kovan migrations

--- migrations/3_deploy_DeFiCustodyRegistry.js
+++ migrations/3_deploy_DeFiCustodyRegistry.js
@@ -1,7 +1,19 @@
 const DeFiCustodyRegistry = artifacts.require("DeFiCustodyRegistry.sol");
 
 module.exports = async (deployer, network, accounts) => {
+  let rayStorage;
+  console.log(network);
+  switch(network) {
+    case "development":
+      rayStorage = "0x8734b14327c94A8252cDb40DBE44a628d38CEBe2";
+      break;
+    case "kovan":
+    case "kovan-fork":
+      rayStorage = "0x39Df6B760C1be5DCD0162E2B0057A963F148B531";
+      break;
+  }
+  console.log(`Deploying with\nRay Storage: ${rayStorage}\nOwner address: ${accounts[0]}`);
   await deployer.deploy(DeFiCustodyRegistry);
   let dc = await DeFiCustodyRegistry.deployed();
-  await dc.init("0x8734b14327c94A8252cDb40DBE44a628d38CEBe2", accounts[0]);
+  let tx = await dc.init(rayStorage, accounts[0]);
 };
