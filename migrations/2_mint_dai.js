2aaf722f70af24c4b3c68b66c3dbc927b0f54496
IlgizZ <ilgiz1996@mail.ru>
2019-11-01 19:48:38 -0700

    test ci

--- migrations/2_mint_dai.js
+++ migrations/2_mint_dai.js
@@ -1,7 +1,14 @@
 const TestDAI = artifacts.require("TestDAI.sol");
 
 module.exports = async (deployer, network, accounts) => {
-  let dai = new web3.eth.Contract(TestDAI.abi, "0x7F6319187249dB5ec845F92ffA3318A9E6604293");
-  let tx = await dai.methods.issueTo(10000).send({from: accounts[0]});
-  console.log(`Minted ${await dai.methods.balanceOf(accounts[0]).call()} DAI for addres ${accounts[0]}1`);
+  let dai = new web3.eth.Contract(
+    TestDAI.abi,
+    "0x7F6319187249dB5ec845F92ffA3318A9E6604293"
+  );
+  let tx = await dai.methods.issueTo(10000).send({ from: accounts[0] });
+  console.log(
+    `Minted ${await dai.methods.balanceOf(accounts[0]).call()} DAI for addres ${
+      accounts[0]
+    }1`
+  );
 };
